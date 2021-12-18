2eb9b05881a48f8a7ee79db69c615d263a755311
xaniar87 <zaniar.ardalan@gmail.com>
2021-12-18 14:50:24 -0700

    PA01 done

--- PA1-WordSearch/src/WordSearch.java
+++ PA1-WordSearch/src/WordSearch.java
@@ -1,5 +1,7 @@
 import java.io.File;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class WordSearch {
     /*
@@ -7,6 +9,8 @@ public class WordSearch {
      * 'MIN_WORD_LENGTH' anywhere you want to refer to it.
      */
     private static final int MIN_WORD_LENGTH = 3;
+    private static int row;
+    private static int col;
 
     public static void main(String[] args) {
         // Remember, to access command-line arguments, you use args[0],
@@ -15,18 +19,76 @@ public class WordSearch {
         try {
             Scanner grid = new Scanner(new File("TestCases/specExample.txt"));
             Scanner vocab = new Scanner(new File("TestCases/dictionary.txt"));
+            Set<String> vocabSet = new HashSet<String>();
+            while (vocab.hasNextLine()) {
+                vocabSet.add(vocab.nextLine().toLowerCase());
+            }
+            int count = 0;
+            while (grid.hasNextLine() && count < 2) { // this while loop only
+                                                      // reads first two rows!!!
+                String line = grid.nextLine();
+                if (count == 0) {
+                    row = Integer.valueOf(line);
+                    count++;
+                } else if (count == 1) {
+                    col = Integer.valueOf(line);
+                    count++;
+                }
+            }
+            String[][] gridArr=new String[row][col];
+            count = 0;
             while (grid.hasNextLine()) {
                 String line = grid.nextLine();
-                System.out.println(line);
+                gridArr[count] = line.split(" ");
+                count++;
             }
-            while (vocab.hasNextLine()) {
-                String word = vocab.nextLine();
-                System.out.println(word);
+            for (int istart = 0; istart < col; istart++) {
+                for (int jstart = 0; jstart < row; jstart++) {
+                    if (col - jstart >= MIN_WORD_LENGTH) {
+                        for (int lengthVertical = MIN_WORD_LENGTH; lengthVertical < col
+                                - jstart + 1; lengthVertical++) {
+                            String str = "";
+                            String reverse = "";
+                            for (int k = 0; k < lengthVertical; k++) {
+                                str += gridArr[istart][jstart + k];
+                                reverse = gridArr[istart][jstart + k] + reverse;
 
+                            }
+                            if (vocabSet.contains(str.toLowerCase())) {
+                                System.out.println("L to R: " + str);
+                            }
+                            if (vocabSet.contains(reverse.toLowerCase())) {
+                                System.out.println("R to L: " + reverse);
+                            }
+
+                        }
+                    }
+                    if (row - istart >= MIN_WORD_LENGTH) {
+                        for (int lengthHorizental = MIN_WORD_LENGTH; lengthHorizental < row
+                                - istart + 1; lengthHorizental++) {
+                            String str = "";
+                            String reverse = "";
+                            for (int k = 0; k < lengthHorizental; k++) {
+                                str += gridArr[istart + k][jstart];
+                                reverse = gridArr[istart + k][jstart] + reverse;
+
+                            }
+                            if (vocabSet.contains(str.toLowerCase())) {
+                                System.out.println("T to D: " + str);
+                            }
+                            if (vocabSet.contains(reverse.toLowerCase())) {
+                                System.out.println("D to T: " + reverse);
+                            }
+
+                        }
+                    }
+
+                }
             }
+
         } catch (Exception IOException) {
             System.out.println(
-                    "grid file IOException occured, plase take care of it");
+                    "file IOException occured, plase take care of it");
         }
     }
 
