02e801917d1532e4113097d8ea80d9f286980937
xaniar87 <zaniar.ardalan@gmail.com>
2021-12-17 18:36:48 -0700

    PA01

---
+++ PA1-WordSearch/src/WordSearch.java
@@ -0,0 +1,27 @@
+import java.io.File;
+import java.util.Scanner;
+
+public class WordSearch {
+    /*
+     * This is how you declare constants in Java. You can now type
+     * 'MIN_WORD_LENGTH' anywhere you want to refer to it.
+     */
+    private static final int MIN_WORD_LENGTH = 3;
+
+    public static void main(String[] args) {
+        // Remember, to access command-line arguments, you use args[0],
+        // args[1],...
+        // See CommandLine.java and Stdin.java in the Class Examples github for examples.
+        try {
+            Scanner grid = new Scanner(new File("TestCases/specExample.txt"));
+            while (grid.hasNextLine()) {
+                String line = grid.nextLine();
+                System.out.println(line);
+            }
+        } catch (Exception IOException) {
+            System.out.println(
+                    "grid file IOException occured, plase take care of it");
+        }
+    }
+
+}
