29b9ed1ca48ce20a1eee5bc0d0bf47e65946abb4
xaniar87 <zaniar.ardalan@gmail.com>
2021-12-19 14:54:48 -0700

    LargestDigit example done, another approach

--- example/src/LargestDigit.java
+++ example/src/LargestDigit.java
@@ -6,30 +6,41 @@ public class LargestDigit {
 		System.out.println(largestDigit(8342116));
 		System.out.println(largestDigit(40109));
 		System.out.println(largestDigit(-163505));
-
-
 	}
 	public static int largestDigit(int n) {
 		if (n<0) {
 			n=n*-1;
 		}
-		if(n<100) {
-			if (n/10>n%10) {
-				return n/10;
-			}else {
-				return n%10;
-			}
+		if(n<10) {
+			return n;
 		}
-		while(n>=100) {
-			int m=n/100;
-			int twoDigit=n%100;
-			if (twoDigit/10>twoDigit%10) {
-				return largestDigit(m*10+twoDigit/10);
-			}else {
-				return largestDigit(m*10+twoDigit%10);
-			}	
+		else {
+			return Math.max(n%10, largestDigit(n/10));	
 		}
-		return 0;
 	}
+
+//	Second Approach (it works fine too)
+//	public static int largestDigit(int n) {
+//		if (n<0) {
+//			n=n*-1;
+//		}
+//		if(n<100) {
+//			if (n/10>n%10) {
+//				return n/10;
+//			}else {
+//				return n%10;
+//			}
+//		}
+//		else {
+//			int m=n/100;
+//			int twoDigit=n%100;
+//			if (twoDigit/10>twoDigit%10) {
+//				return largestDigit(m*10+twoDigit/10);
+//			}else {
+//				return largestDigit(m*10+twoDigit%10);
+//			}	
+//		}
+//	}
+	
 
 }
